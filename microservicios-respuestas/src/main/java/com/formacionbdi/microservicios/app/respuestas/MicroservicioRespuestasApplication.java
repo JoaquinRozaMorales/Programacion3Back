131e67305df80d82cf97007139a2ed6d7f8fe94a
Joaquín Roza <joacoroza@gmail.com>
2021-02-26 15:27:12 -0300

    primer commit

---
+++ microservicios-respuestas/src/main/java/com/formacionbdi/microservicios/app/respuestas/MicroservicioRespuestasApplication.java
@@ -0,0 +1,20 @@
+package com.formacionbdi.microservicios.app.respuestas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication
+public class MicroservicioRespuestasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioRespuestasApplication.class, args);
+	}
+
+}
